b918b828919bf416be82622dede3754d33918e1b
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-10-06 20:01:18 -0400

    add meats

--- app/src/main/java/comp304/josephharrisonlim/assignment2/foods/MeatsActivity.kt
+++ app/src/main/java/comp304/josephharrisonlim/assignment2/foods/MeatsActivity.kt
@@ -1,13 +1,55 @@
 package comp304.josephharrisonlim.assignment2.foods
 
 import android.os.Bundle
+import android.widget.CheckBox
 import comp304.josephharrisonlim.assignment2.AbstractOptionsMenuActivity
 import comp304.josephharrisonlim.assignment2.R
+import comp304.josephharrisonlim.assignment2.data.Food
+import comp304.josephharrisonlim.assignment2.data.FoodDatabase
+import comp304.josephharrisonlim.assignment2.data.FoodType
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_meats.*
 
 class MeatsActivity : AbstractOptionsMenuActivity() {
+
+    private val meatItems: Array<Food> = arrayOf(
+            Food("Chicken Wings", 9.00, FoodType.GRAINS.name),
+            Food("Striploin Steaks", 12.00, FoodType.GRAINS.name),
+            Food("Bacon", 5.00, FoodType.GRAINS.name)
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meats)
+        meatItems.map { food ->
+            val btn = CheckBox(this)
+            btn.text = food.name
+            FoodDatabase.getInstance(this)!!.foodDataDao().getAll()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe { foods ->
+                        for (f in foods) {
+                            if (f == food) {
+                                btn.isChecked = true
+                            }
+                        }
+                    }
+
+            btn.setOnCheckedChangeListener { _, isChecked ->
+                Single.fromCallable {
+                    val dao = FoodDatabase.getInstance(this)!!.foodDataDao()
+                    if (isChecked) {
+                        dao.insert(food)
+                    } else {
+                        dao.delete(food)
+                    }
+                }.subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe()
+            }
+            meatsLayout.addView(btn)
+        }
     }
 }
