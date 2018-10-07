6f5f7c7e9d78a9b8caa70dd6eb2b449905d56c89
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-10-07 16:39:21 -0400

    add payment activity content

--- app/src/main/java/comp304/josephharrisonlim/assignment2/PaymentActivity.kt
+++ app/src/main/java/comp304/josephharrisonlim/assignment2/PaymentActivity.kt
@@ -1,12 +1,35 @@
 package comp304.josephharrisonlim.assignment2
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_payment.*
 
 class PaymentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment)
+    }
+
+    fun proceedToOrder(view: View) {
+        when (view.id) {
+            R.id.proceedToFinalOrderBtn -> {
+                val bundle = Bundle()
+                when (paymentOptionGroup.checkedRadioButtonId) {
+                    0 -> {
+                        bundle.putInt("T_PYMT", 0)
+                    }
+                    1 -> {
+                        bundle.putInt("T_PYMT", 1)
+                    }
+                    2 -> {
+                        bundle.putInt("T_PYMT", 2)
+                    }
+                }
+                startActivity(Intent(this, FinalOrderActivity::class.java), bundle)
+            }
+        }
     }
 }
