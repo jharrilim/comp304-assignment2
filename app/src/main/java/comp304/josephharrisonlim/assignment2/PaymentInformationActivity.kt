94aeb7b5d29c4fcbb67ada63e421cb717239ea1d
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-10-08 00:48:05 -0400

    add pickers and fields

--- app/src/main/java/comp304/josephharrisonlim/assignment2/PaymentInformationActivity.kt
+++ app/src/main/java/comp304/josephharrisonlim/assignment2/PaymentInformationActivity.kt
@@ -2,11 +2,18 @@ package comp304.josephharrisonlim.assignment2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_payment_information.*
 
 class PaymentInformationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment_information)
+//        val adapter = ArrayAdapter.createFromResource(this,
+//                R.array.months, android.R.layout.simple_spinner_item)
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//        monthSpinner.adapter = adapter
     }
+
 }
